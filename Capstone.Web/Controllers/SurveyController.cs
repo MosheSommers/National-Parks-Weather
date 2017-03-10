7176e030f1d711253f97b375492c263a9e08803e
Rachel Funk <rachelleahlong@gmail.com>
2017-03-10 12:31:44 -0500

    started to make survey model and controller

---
+++ Capstone.Web/Controllers/SurveyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("SurveyForm");
+        }
+
+        [HttpPost]
+        public ActionResult SurveyForm(Survey survey)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Capstone.Web/Models/Survey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Survey
+    {
+        public string ParkCode { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        public string State { get; set; }
+        public string Activity { get; set; }
+    }
+
+}
